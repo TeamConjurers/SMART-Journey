e04c688685f9652f942dc1186e78d978d804daa0
TeamConjurers <96539310+TeamConjurers@users.noreply.github.com>
2022-01-14 22:09:59 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,16 +1,12 @@
-<?php 
-// DB credentials.
-define('DB_HOST','remotemysql.com');
-define('DB_USER','8CIcE1JU9U');
-define('DB_PASS','iM3TJxZKfY');
-define('DB_NAME','8CIcE1JU9U');
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
