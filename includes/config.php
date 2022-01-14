24a9fdb5c585a88e355262e2f8043673e53a3827
TeamConjurers <96539310+TeamConjurers@users.noreply.github.com>
2022-01-14 22:15:52 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+$cleardb_username = $cleardb_url["b0736b70925e02"];
+$cleardb_password = $cleardb_url["3c2eb69c"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
