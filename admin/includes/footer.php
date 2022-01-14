c8f88804bf62ccb729a1cc5e57726529f3e5c048
TeamConjurers <96539310+TeamConjurers@users.noreply.github.com>
2022-01-14 18:51:40 +0530

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,4 +1,4 @@
 <div class="copyrights">
-	 <p>© 2022 Team Conjurers. All Rights Reserved |  <a href="https://cloudtest12345678.herokuapp.com/index.php
+	 <p>© 2022 Team Conjurers. All Rights Reserved |  <a href="https://smartjourney2022.herokuapp.com/
 ">SMART Journey</a> </p>
 </div>	
