06bf3a85b23666effd7021db500f65898c5759a3
TeamConjurers <96539310+TeamConjurers@users.noreply.github.com>
2022-01-14 22:50:57 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -4,7 +4,7 @@ $cleardb_url = parse_url(getenv("mysql://b0736b70925e02:3c2eb69c@us-cdbr-east-05
 $cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
 $cleardb_username = $cleardb_url["b0736b70925e02"];
 $cleardb_password = $cleardb_url["3c2eb69cpass"];
-$cleardb_db = substr($cleardb_url["heroku_db0ef09713264fd"],1);
+$cleardb_db = substr($cleardb_url["http://localhost/phpmyadmin/index.php?route=/database/structure&server=2&server=2&db=heroku_db0ef09713264fd"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
