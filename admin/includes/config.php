b03f793160b494714161a9dfe76145282b16dd74
TeamConjurers <96539310+TeamConjurers@users.noreply.github.com>
2022-01-14 22:56:11 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,13 +1,18 @@
-<?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("mysql://b0736b70925e02:3c2eb69c@us-cdbr-east-05.cleardb.net/heroku_db0ef09713264fd?reconnect=true"));
-$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
-$cleardb_username = $cleardb_url["b0736b70925e02"];
-$cleardb_password = $cleardb_url["3c2eb69cpass"];
-$cleardb_db = substr($cleardb_url["http://localhost/phpmyadmin/index.php?route=/database/structure&server=2&server=2&db=heroku_db0ef09713264fd"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+<?php 
+// DB credentials.
+define('DB_HOST','remotemysql.com');
+define('DB_USER','8CIcE1JU9U');
+define('DB_PASS','iM3TJxZKfY');
+define('DB_NAME','8CIcE1JU9U');
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
 ?>
+
 
