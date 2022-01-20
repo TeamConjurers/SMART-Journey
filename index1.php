511b06e9f2aee8a7d6ff618dad82f157731523ca
TeamConjurers <96539310+TeamConjurers@users.noreply.github.com>
2022-01-20 22:23:43 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -178,6 +178,20 @@ foreach($results as $result)
 	</div>
 <body>
 	<br>
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61e98686b84f7301d32c11d5/1fps3avns';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
     <?php include('includes/footer.php');?>
 
 <!-- signup -->
